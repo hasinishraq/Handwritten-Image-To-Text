abe2de17e891cd11529e7fcc6242683c120bd4df
Hasin Ishraq <146731466+hasinishraq@users.noreply.github.com>
2024-09-09 17:17:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 const storage = multer.memoryStorage();
